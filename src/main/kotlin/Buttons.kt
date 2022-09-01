7450372582edd500d165c34fef2b4d7172a26829
snakemr <snakemr@gmail.com>
2022-09-01 09:25:20 +0300

    PointerEventButton readme

--- src/main/kotlin/Buttons.kt
+++ src/main/kotlin/Buttons.kt
@@ -1,9 +1,14 @@
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.OutlinedButton
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.awtEventOrNull
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import java.awt.event.MouseEvent
@@ -13,22 +18,23 @@ import java.awt.event.MouseEvent
 fun PointerEventButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-//    enabled: Boolean,
-//    interactionSource: MutableInteractionSource,
-//    elevation: ButtonElevation?,
-//    shape: Shape,
-//    border: BorderStroke?,
-//    colors: ButtonColors,
-//    contentPadding: PaddingValues,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = ButtonDefaults.outlinedBorder,
+    colors: ButtonColors = ButtonDefaults.outlinedButtonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     onDoubleClick: (() -> Unit)? = null,
     onRightClick: (() -> Unit)? = null,
     content: @Composable RowScope.() -> Unit
-) = OutlinedButton({}, modifier.onPointerEvent(PointerEventType.Press) {
+) : Unit = OutlinedButton({}, modifier.onPointerEvent(PointerEventType.Press) {
     it.awtEventOrNull?.run {
         if (button == MouseEvent.BUTTON1)
             if (clickCount == 2) onDoubleClick?.invoke() else onClick()
         else if (button == MouseEvent.BUTTON3)
             onRightClick?.invoke()
     }},
-    content = content
+    enabled, interactionSource, elevation, shape, border, colors, contentPadding,
+    content
 )
